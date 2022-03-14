13ed97451963bc45e52a48ccb836fa20dd533617
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-03-14 21:07:28 +0200

    chore(config): polished configs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import 'module-alias/register';
 import express from 'express';
 import { greeterRouter } from '@controllers/';
+import 'dotenv/config';
 
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 5000; // default port to listen
 
+app.get('/', (req, res) => {
+    return res.status(200).json({message:"Welcome to the App API"});
+});
 app.use('/api', greeterRouter);
 
 app.listen(port, () => {
